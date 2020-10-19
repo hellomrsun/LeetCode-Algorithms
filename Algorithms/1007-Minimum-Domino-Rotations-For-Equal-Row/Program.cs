23e4eaee653e148cc8b2534811134e7166ea5a36
hellomrsun <23157401+hellomrsun@users.noreply.github.com>
2020-10-19 21:59:19 +0200

    Add Minimum-Domino-Rotations-For-Equal-Row

---
+++ Algorithms/1007-Minimum-Domino-Rotations-For-Equal-Row/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1007_Minimum_Domino_Rotations_For_Equal_Row
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[6] {2,1,2,4,2,2};
+            int[] b = new int[6] {5,2,6,2,3,2};
+            var r = new Solution().MinDominoRotations(a, b);
+            Console.WriteLine("The minimum domino rotations is:" + r);
+        }
+    }
+}
--- Algorithms/188-Best-Time-to-Buy-and-Sell-Stock-IV/Program.cs
+++ Algorithms/188-Best-Time-to-Buy-and-Sell-Stock-IV/Program.cs
@@ -7,7 +7,7 @@ namespace _188_Best_Time_to_Buy_and_Sell_Stock_IV
         static void Main(string[] args)
         {
            var result = new Solution().MaxProfit(2, new int[]{3,2,6,5,0,3});
-           Console.WriteLine(result);
+           Console.WriteLine("the maximum profit is:" + result);
         }
     }
 }
