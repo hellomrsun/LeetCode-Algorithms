eabb2440459d2ca0e900495004b8a4f4270ec431
hellomrsun <23157401+hellomrsun@users.noreply.github.com>
2020-10-25 11:23:55 +0100

    Add solutions

---
+++ Algorithms/1510-Stone-Game-IV/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1510_Stone_Game_IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Solution().WinnerSquareGame(17);
+            Console.WriteLine("17 result:" + r);
+        }
+    }
+}
---
+++ Algorithms/456-132-Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _456_132_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[5] { 3, 5, 0, 3, 4 };
+            var result = new Solution().Find132pattern(arr);
+            Console.WriteLine($"{string.Join(",", arr)} is 132 pattern?:{result}");
+
+            var arr2 = new int[8] { 5, 6, 7, 10, 9, 12, 13, 14 };
+            var result2 = new Solution().Find132pattern(arr2);
+            Console.WriteLine($"{string.Join(",", arr2)} is 132 pattern?:{result2}");
+        }
+    }
+}
