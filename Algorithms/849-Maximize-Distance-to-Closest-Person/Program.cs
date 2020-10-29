c9b057edd1352da696333768b16ed04818c782c0
hellomrsun <23157401+hellomrsun@users.noreply.github.com>
2020-10-29 22:41:53 +0100

    Add solution

---
+++ Algorithms/849-Maximize-Distance-to-Closest-Person/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _849_Maximize_Distance_to_Closest_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[7]{1,0,0,0,1,0,1};
+            var res = new Solution().MaxDistToClosest(arr);
+            Console.WriteLine(res);
+        }
+    }
+}
