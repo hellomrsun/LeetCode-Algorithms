e2a7692c94580857311c520aecd6ecef46e58e6e
hellomrsun <23157401+hellomrsun@users.noreply.github.com>
2020-11-03 20:53:25 +0100

    Add solutions

---
+++ Algorithms/1446-Consecutive-Characters/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1446_Consecutive_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = new Solution().MaxPower("Hello World!");
+            Console.WriteLine("Result is: " + res);
+        }
+    }
+}
---
+++ Algorithms/147-Insertion-Sort-List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace _147_Insertion_Sort_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node = new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3))));
+            var res = new Solution().InsertionSortList(node);
+            var list = new List<int>();
+            while(res != null){
+                list.Add(res.val);
+                res = res.next;
+            }
+            Console.WriteLine(string.Join("->", list));
+        }
+    }
+}
