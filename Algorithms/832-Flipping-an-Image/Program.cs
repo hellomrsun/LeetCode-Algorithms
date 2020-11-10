745306834a1a67370854bafcacf565e068455411
hellomrsun <23157401+hellomrsun@users.noreply.github.com>
2020-11-10 19:43:34 +0100

    Add Solution

---
+++ Algorithms/832-Flipping-an-Image/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _832_Flipping_an_Image
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[3][]{
+                new int[3] {1,1,0},
+                new int[3] {1,0,1},
+                new int[3] {0,0,0},
+            };
+            var result = new Solution().FlipAndInvertImage(arr);
+            Console.WriteLine(result);
+        }
+    }
+}
