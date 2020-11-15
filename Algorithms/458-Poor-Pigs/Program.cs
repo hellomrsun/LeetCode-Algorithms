3ce38d8bd260c4f08f502ee66d076f74fca2a67d
hellomrsun <23157401+hellomrsun@users.noreply.github.com>
2020-11-15 22:42:53 +0100

    Add C# solutions

---
+++ Algorithms/458-Poor-Pigs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _458_Poor_Pigs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Algorithms/938-Range-Sum-of-BST/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _938_Range_Sum_of_BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node =
+                new TreeNode(10,
+                    new TreeNode(5,
+                        new TreeNode(3, new TreeNode(1)),
+                        new TreeNode(7, new TreeNode(6))),
+                    new TreeNode(15, new TreeNode(13), new TreeNode(18)));
+            var res = new Solution().RangeSumBST(node, 6, 10);
+            Console.WriteLine(res);
+        }
+    }
+}
